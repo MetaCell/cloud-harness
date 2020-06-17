c0684828a5522ae29adfef329ecf783012414bc4
Zoran Sinnema <zsinnema@gmail.com>
2020-06-17 10:30:15 +0200

    chore: add test if app has the harness.sentry setting set to True, if not or False then return '' indicating no Sentry DSN
    see #35 osb

--- applications/chservice/chservice/controllers/sentry_controller.py
+++ applications/chservice/chservice/controllers/sentry_controller.py
@@ -8,27 +8,31 @@ from chservice import util
 from chservice.repository.sentry import get_token, get_dsn, SentryProjectNotFound
 
 from cloudharness.utils.env import get_sentry_service_cluster_address
+from cloudharness.utils.config import CloudharnessConfig as conf
 
 
 def getdsn(appname):  # noqa: E501
-    """Gets the Sentry DSN for a given application
-
-    Gets the Sentry DSN for a given application # noqa: E501
+    """
+    Gets the Sentry DSN for a given application
 
     :param appname: 
     :type appname: str
 
     :rtype: str
     """
-    try:
-        dsn =  get_dsn(appname)
-    except SentryProjectNotFound as e:
-        # if project not found, create one
-        sentry_api_token = get_token()
-        headers = {'Authorization': 'Bearer ' + sentry_api_token}
-        url = get_sentry_service_cluster_address() + f'/api/0/teams/sentry/sentry/projects/'
-        data = {'name' : appname}
-        response = requests.post(url, data, headers=headers, verify=False)
-        dsn =  get_dsn(appname)
+    ch_app = conf.get_application_by_filter(name=appname)[0]
+    if getattr(ch_app.harness, 'sentry', False) == True:
+        try:
+            dsn =  get_dsn(appname)
+        except SentryProjectNotFound as e:
+            # if project not found, create one
+            sentry_api_token = get_token()
+            headers = {'Authorization': 'Bearer ' + sentry_api_token}
+            url = get_sentry_service_cluster_address() + f'/api/0/teams/sentry/sentry/projects/'
+            data = {'name' : appname}
+            response = requests.post(url, data, headers=headers, verify=False)
+            dsn =  get_dsn(appname)
+    else:
+        dsn = ''
 
     return {'dsn': dsn}
