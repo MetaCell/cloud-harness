a5bef61390a661f305402c40511aca0d64d8193d
Zoran Sinnema <zsinnema@gmail.com>
2020-12-04 15:15:21 +0100

    #115 chore: add/update/delete kc user attributes

--- libraries/cloudharness-common/cloudharness/auth/keycloak/__init__.py
+++ libraries/cloudharness-common/cloudharness/auth/keycloak/__init__.py
@@ -327,3 +327,49 @@ class AuthClient():
             self._get_keycloak_user_id(),
             client_name,
             role)
+
+    def user_add_update_attribute(self, user_id, attribute_name, attribute_value):
+        """
+        Adds or when exists updates the attribute to/of the User with the attribute value
+
+        param user_id: id of the user
+        param attribute_name: name of the attribute to add/update
+        param attribute_value: value of the attribute
+        :return: boolean True on success
+        """
+        admin_client = self.get_admin_client()
+        try:
+            user = self.get_user(user_id)
+            attributes = user.get('attributes', [])
+            attributes[attribute_name] = attribute_value
+            admin_client.update_user(
+                user_id,
+                {
+                    'attributes': attributes
+                })
+            return True
+        except:
+            return False
+
+    def user_delete_attribute(self, user_id, attribute_name):
+        """
+        Deletes the attribute to/of the User with the attribute value
+
+        param user_id: id of the user
+        param attribute_name: name of the attribute to delete
+        :return: boolean True on success
+        """
+        admin_client = self.get_admin_client()
+        try:
+            user = self.get_user(user_id)
+            attributes = user.get('attributes', None)
+            if attributes:
+                del attributes[attribute_name]
+            admin_client.update_user(
+                user_id,
+                {
+                    'attributes': attributes
+                })
+            return True
+        except:
+            return False
