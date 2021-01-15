8e5d0e6bad95e000608c2eaa7316947788c45e87
Filippo Ledda <filippo@metacell.us>
2021-01-15 13:03:13 +0100

    Improve extract-download chmod

--- applications/workflows/tasks/extract-download/main.py
+++ applications/workflows/tasks/extract-download/main.py
@@ -1,10 +1,8 @@
 import urllib.request
 import sys
-import logging
+from cloudharness import log
 import os, stat
 from pathlib import Path
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 assert len(sys.argv) > 2, 'Arguments not specified. Cannot download'
 
@@ -21,28 +19,40 @@ Path(download_path).mkdir(parents=True, exist_ok=True)
 
 dest = os.path.join(download_path, url.split('/')[-1])
 
-logging.info("Downloading {} to {}".format(url, dest))
+log.info("Downloading {} to {}".format(url, dest))
 
 urllib.request.urlretrieve(url, dest)
 
 # test to see if the file is an zip archive
 import mimetypes
+
 mime = mimetypes.MimeTypes().guess_type(dest)[0]
+
+mode_file = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH  # r+w g+o
+mode_dir = mode_file | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH  # for dirs also execute
 if mime == "application/zip":
     # extract it to the download_path folder
     import zipfile
+
     with zipfile.ZipFile(dest, 'r') as zip_ref:
         zip_ref.extractall(download_path)
-        os.remove(dest)
+        for fname in zip_ref.namelist():
+            name = os.path.join(download_path, fname)
+            if os.path.isdir(name):
+                try:
+                    os.chmod(name, mode_dir)
+                except:
+                    log.warning("Cannot change folder permissions: %s", name, exc_info=True)
+            else:
+                try:
+                    # log.info(f"chmod {dirpath}/{filename} to {mode_file}")
+                    os.chmod(name, mode_file)
+                except:
+                    log.warning("Cannot change file permissions: %s", name, exc_info=True)
+    os.remove(dest)
+else:
+    try:
+        os.chmod(dest, mode_file)
+    except:
+        log.warning("Cannot change file permissions: %s", dest, exc_info=True)
 
-# TODO: may be a bit too drastic, may be only change the destination but how to handle the zip files where if there is a folder it is unknow
-mode_file = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH # r+w g+o
-mode_dir = mode_file | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH # for dirs also execute
-for dirpath, dirnames, filenames in os.walk(download_path):
-    os.chmod(dirpath, mode_dir)
-    for filename in filenames:
-        try:
-            logging.info(f"chmod {dirpath}/{filename} to {mode_file}")
-            os.chmod(os.path.join(dirpath, filename), mode_file)
-        except:
-            logging.error("Cannot change file permissions: %s", filename, exc_info=True)
