6ab63062fe384d3e7b0ff8140264b12186ad6383
Lucas Rebscher <lucas@metacell.us>
2021-02-01 10:02:44 +0100

    #137 Extract backup script to separate files
    * Use .Files object to inject backup.sh

---
+++ utilities/cloudharness_utilities/deployment-configuration/helm/files/db-mongo-backup.sh
@@ -0,0 +1,36 @@
+/bin/bash << 'EOF'
+KEEP_DAYS=${BACKUP_KEEP_DAYS}
+KEEP_WEEKS=`expr $(((${BACKUP_KEEP_WEEKS} * 7) + 1))`
+KEEP_MONTHS=`expr $(((${BACKUP_KEEP_MONTHS} * 31) + 1))`
+
+mkdir -p "${BACKUP_DIR}/daily/" "${BACKUP_DIR}/weekly/" "${BACKUP_DIR}/monthly/"
+DFILE="${BACKUP_DIR}/daily/`date +%Y-%m-%d-%H%M%S`${BACKUP_SUFFIX}"
+WFILE="${BACKUP_DIR}/weekly/`date +%G-%V`${BACKUP_SUFFIX}"
+MFILE="${BACKUP_DIR}/monthly/`date +%Y-%m`${BACKUP_SUFFIX}"
+
+# Dump mongo database
+/usr/bin/mongodump -h $DB_HOST -u $DB_USER -p $DB_PASS --archive=$DFILE --gzip
+ls backups/*
+
+# Use hardlink instead of copy to save space
+if [ -d "${DFILE}" ]; then
+    WFILENEW="${WFILE}-new"
+    MFILENEW="${MFILE}-new"
+    rm -rf "${WFILENEW}" "${MFILENEW}"
+    mkdir "${WFILENEW}" "${MFILENEW}"
+    ln -f "${DFILE}/"* "${WFILENEW}/"
+    ln -f "${DFILE}/"* "${MFILENEW}/"
+    rm -rf "${WFILE}" "${MFILE}"
+    mv -v "${WFILENEW}" "${WFILE}"
+    mv -v "${MFILENEW}" "${MFILE}"
+else
+    ln -vf "${DFILE}" "${WFILE}"
+    ln -vf "${DFILE}" "${MFILE}"
+fi
+
+# Clean old files
+echo "Cleaning backups older than ${KEEP_DAYS} ..."
+find "${BACKUP_DIR}/daily" -maxdepth 1 -mtime +${KEEP_DAYS} -name "*${BACKUP_SUFFIX}" -print -delete
+find "${BACKUP_DIR}/weekly" -maxdepth 1 -mtime +${KEEP_WEEKS} -name "*${BACKUP_SUFFIX}" -print -delete
+find "${BACKUP_DIR}/monthly" -maxdepth 1 -mtime +${KEEP_MONTHS} -name "*${BACKUP_SUFFIX}" -print -delete
+EOF
