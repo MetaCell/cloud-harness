f21c17300246a29512da3bbac1197d0dcb84a374
Lucas Rebscher <luc.rebscher@gmail.com>
2021-02-16 15:58:52 +0100

    #152 Fix PEP 8 violations

--- libraries/cloudharness-common/cloudharness/workflows/utils.py
+++ libraries/cloudharness-common/cloudharness/workflows/utils.py
@@ -5,17 +5,19 @@ from cloudharness.utils.env import get_variable
 
 WORKFLOW_NAME_VARIABLE_NAME = "CH_WORKFLOW_NAME"
 
-
 SHARED_DIRECTORY_VARIABLE_NAME = "shared_directory"
+
 
 def get_workflow_name():
     """Get the workflow name from inside a workflow"""
     name = get_variable(WORKFLOW_NAME_VARIABLE_NAME)
     remove = name.split("-")[-1]
-    return name[0:-len(remove)-1]
+    return name[0:-len(remove) - 1]
+
 
 def get_shared_directory():
     return os.getenv(SHARED_DIRECTORY_VARIABLE_NAME)
+
 
 def notify_queue(queue, message):
     client = EventClient(queue)
