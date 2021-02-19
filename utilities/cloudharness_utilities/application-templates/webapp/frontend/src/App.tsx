badcd8eefd56ea194c2ad169226f20e073bd0123
Filippo Ledda <filippo@metacell.us>
2021-02-19 12:01:21 +0100

    #199 Update application generator templates

--- utilities/cloudharness_utilities/application-templates/webapp/frontend/src/App.tsx
+++ utilities/cloudharness_utilities/application-templates/webapp/frontend/src/App.tsx
@@ -9,6 +9,7 @@ const Main = () => (
       <img src="/assets/icon.png" />
       <h1>Sample React application is working!</h1>
       <RestTest />
+      <p>See api documentation <a href="/api/ui">here</a></p>
     </>
 );
 
