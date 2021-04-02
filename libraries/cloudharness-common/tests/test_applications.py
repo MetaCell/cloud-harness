533c6c064694b1355864b56bd197a03f83347071
Filippo Ledda <filippo@metacell.us>
2021-04-02 20:21:47 +0200

    #267 Fix unit tests for image names

--- libraries/cloudharness-common/tests/test_applications.py
+++ libraries/cloudharness-common/tests/test_applications.py
@@ -68,13 +68,13 @@ def test_get_configuration():
     assert not uut.is_auto_service()
     assert uut.is_auto_deployment()
     assert uut.is_sentry_enabled()
-
+    assert uut.image_name == 'image1-name'
     uut = get_configuration('app2')
     assert uut.name == 'app2'
     assert not uut.is_auto_service()
     assert not uut.is_auto_deployment()
     assert uut.is_sentry_enabled()
-    assert uut.image_name == 'image1-name'
+
 
     # uut = get_configuration('app2sub') # FIXME this should work
     uut = uut.subapp
