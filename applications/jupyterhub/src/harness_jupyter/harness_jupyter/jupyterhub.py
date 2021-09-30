0d21041608a83bf8d6c5feb01b73b7e6190cd68e
Zoran Sinnema <zsinnema@gmail.com>
2021-09-30 14:41:08 +0200

    #327 chore: added support for application specific node pool selector

--- applications/jupyterhub/src/harness_jupyter/harness_jupyter/jupyterhub.py
+++ applications/jupyterhub/src/harness_jupyter/harness_jupyter/jupyterhub.py
@@ -45,9 +45,41 @@ def change_pod_manifest(self: KubeSpawner):
                     #       cpu_limit: 3
                     #       mem_guarantee: 4G
                     #       mem_limit: 8G
-                    if 'spawnerExtraConfig' in harness['jupyterhub']:
-                        for k, v in harness['jupyterhub']['spawnerExtraConfig'].items():
+                    #       node_selector:             # optional node pool for these pods
+                    #         - key: ch/nodepool
+                    #           operator: In           # In, NotIn, Exists, DoesNotExist. Gt, and Lt.
+                    #           values: pool-jupyter
+                    #           matchPurpose: require  # require | prefer | ignore
+                    spawnerExtraConfig = getattr(harness['jupyterhub'], 'spawnerExtraConfig', None)
+                    if spawnerExtraConfig:
+                        for k, v in spawnerExtraConfig.items():
                             setattr(self, k, v)
+                        node_selectors = getattr(self, 'node_selector', None)  
+                        if node_selectors:
+                            for node_selector in node_selectors:
+                                labels[node_selector.key] = node_selector.values
+                                node_selector = dict(
+                                    matchExpressions=[
+                                        dict(
+                                            key=node_selector.key,
+                                            operator=node_selector.operator,
+                                            values=[node_selector.values],
+                                        )
+                                    ],
+                                )
+                                if node_selector.matchPurpose == 'prefer':
+                                    self.node_affinity_preferred.append(
+                                        dict(
+                                            weight=100,
+                                            preference=node_selector,
+                                        ),
+                                    )
+                                elif match_node_purpose == 'require':
+                                    self.node_affinity_required.append(node_selector)
+                                elif match_node_purpose == 'ignore':
+                                    pass
+                                else:
+                                    raise ValueError("Unrecognized value for matchNodePurpose: %r" % match_node_purpose)
 
                     # check if there is an applicationHook defined in the values.yaml
                     # if so then execute the applicationHook function with "self" as parameter
