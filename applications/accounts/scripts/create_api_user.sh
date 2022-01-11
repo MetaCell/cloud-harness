a71b59e462477ba727904aebf84451372e6e75dc
Zoran Sinnema <zsinnema@gmail.com>
2022-01-11 16:23:39 +0100

    #362 chore: moved creation of accounts-api secret to helm template and updated init accounts script

--- applications/accounts/scripts/create_api_user.sh
+++ applications/accounts/scripts/create_api_user.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
 NAMESPACE=${CH_ACCOUNTS_REALM}
-USERNAME=admin_api
-PASSWORD=$(echo $RANDOM | md5sum | head -c 20; echo;)
+USERNAME=$(cat /opt/cloudharness/resources/auth/api_user_username)
+PASSWORD=$(cat /opt/cloudharness/resources/auth/api_user_password)
 
-api_user_secret_exists=$(kubectl -n ${NAMESPACE} get secret accounts-api >/dev/null; echo $?)
-
-if [ "${api_user_secret_exists}" -ne "0" ];
-then
 set -e
 echo Creating API user
 
 # create the user and reload keycloak
 /opt/jboss/keycloak/bin/add-user-keycloak.sh -u ${USERNAME} -p ${PASSWORD}
-
-kubectl create secret generic -n ${NAMESPACE} accounts-api \
-  --from-literal=api_user_username=$(echo -n "${USERNAME}" | tr -d '\n') \
-  --from-literal=api_user_password=$(echo -n "${PASSWORD}" | tr -d '\n')
-
-fi
