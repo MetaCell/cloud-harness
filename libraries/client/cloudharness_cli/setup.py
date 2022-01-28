02f0ce00a9d82fe0819dca03dbe2be5152e5fe40
Dario Del Piano <dario@metacell.us>
2022-01-28 12:27:20 +0000

    #392 setting back version

--- libraries/client/cloudharness_cli/setup.py
+++ libraries/client/cloudharness_cli/setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "cloudharness-cli"
-VERSION = "1.0.0"
+VERSION = "0.4.0"
 # To install the library, run the following
 #
 # python setup.py install
--- libraries/cloudharness-common/setup.py
+++ libraries/cloudharness-common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 NAME = "cloudharness"
-VERSION = "1.0.0"
+VERSION = "0.4.0"
 # To install the library, run the following
 #
 # python setup.py install
