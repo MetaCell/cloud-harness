2e1dee3f9369c1c5fa006cc9627866c4ad03c0dc
Filippo Ledda <filippo@metacell.us>
2022-03-10 10:15:05 +0100

    #451 fix tests, requirements

--- libraries/models/setup.py
+++ libraries/models/setup.py
@@ -2,9 +2,18 @@
 
 import sys
 from setuptools import setup, find_packages
+from os.path import join, dirname as dn, realpath
+
+
+HERE = dn(realpath(__file__))
 
 NAME = "cloudharness_model"
 VERSION = "1.0.0"
-REQUIREMENTS = [l[0:-1].replace(" ", "") for l in open("requirements.txt") if "#" not in l]
+REQUIREMENTS = [
+    "swagger-ui-bundle >= 0.0.2",
+    "python_dateutil >= 2.6.0",
+    "setuptools >= 21.0.0",
+    "pyhumps"
+]
 print(REQUIREMENTS)
 setup(name=NAME, version=VERSION, install_requires=REQUIREMENTS)
