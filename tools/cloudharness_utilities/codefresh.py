2b1e55bf0e164c638653d1efe590d4f0e1456937
Filippo Ledda <filippo@metacell.us>
2022-05-06 18:09:03 +0200

    #451 backwards compatibility fix

--- tools/cloudharness_utilities/codefresh.py
+++ tools/cloudharness_utilities/codefresh.py
@@ -195,7 +195,7 @@ def codefresh_app_build_spec(app_name, app_context_path, dockerfile_path="Docker
         dep_list = helm_values.apps[values_key].harness.dependencies.build
         dependencies = [f"{d.upper().replace('-', '_')}=${{{{REGISTRY}}}}/{get_image_name(d, base_name)}:{build['tag']}" for
                         d in dep_list]
-    except KeyError:
+    except (KeyError, AttributeError):
         dependencies = [f"{d.upper().replace('-', '_')}=${{{{REGISTRY}}}}/{get_image_name(d, base_name)}:{build['tag']}" for
                         d in helm_values['task-images']]
     build['build_arguments'].extend(dependencies)
