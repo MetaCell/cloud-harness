ca993562d1e385ef90e524e59e0e9c0fe149c800
Filippo Ledda <filippo@metacell.us>
2022-06-14 15:50:22 +0200

    #248 rename e2e test project

---
+++ test/test-e2e/README.md
@@ -0,0 +1,4 @@
+# End to end test base application
+
+This application is used as a base app and image to run end to end tests
+with Jest and Puppeteer.
---
+++ test/test-e2e/__tests__/common.spec.ts
@@ -0,0 +1,41 @@
+import * as puppeteer from "puppeteer";
+
+let page: any;
+let browser: any;
+
+describe("Sandbox", () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ args: ['--no-sandbox'], })
+
+    page = await browser.newPage();
+    page.on('pageerror', ({ message }: any) => expect(message).toBe(""))
+      .on('response', (response: any) => {
+        if (response.status() >= 400) {
+          console.log(`${response.status()} ${response.url()}`)
+        }
+        expect(response.status()).toBeLessThan(400)
+      })
+
+      .on('requestfailed', (request: any) => {
+        console.log(`${request.failure().errorText} ${request.url()}`);
+        expect(request.failure().errorText).toBe("")
+      })
+    await page
+      .goto(process.env.APP_URL, {
+        waitUntil: "networkidle0",
+      })
+      .catch(() => { });
+  });
+
+  afterAll(() => {
+    if (!page.isClosed()) {
+      browser.close();
+    }
+  });
+
+  test("Check page", async () => {
+
+    await page.waitForSelector("body");
+
+  });
+});
