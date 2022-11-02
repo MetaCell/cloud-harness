0e0a8bb1301dcc52be8057c4b60949b1128467b9
Zoran Sinnema <zsinnema@gmail.com>
2022-11-02 14:53:30 +0100

    #598 feat: add storageclass as param to create pvc

--- libraries/cloudharness-common/cloudharness/service/pvc.py
+++ libraries/cloudharness-common/cloudharness/service/pvc.py
@@ -6,16 +6,55 @@ from cloudharness.utils.config import CloudharnessConfig as conf
 from cloudharness.utils import dict_merge
 
 
-def _get_api():
+def _get_client():
     try:
         configuration = kubernetes.config.load_incluster_config()
     except:
         configuration = kubernetes.config.load_kube_config()
-    api_instance = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(configuration))
-    return api_instance
+    return kubernetes.client.ApiClient(configuration)
 
 
-def create_persistent_volume_claim(name, size, logger, **kwargs):
+def _get_corev1_api():
+    return kubernetes.client.CoreV1Api(_get_client())
+
+
+def _get_default_storage_class() -> str:
+    """
+    Default storage class
+
+    :return:    Returns the default storage class name, if exists.
+                If not, it returns the first storage class.
+                If there are not storage classes, returns None
+    """
+
+    storagev1 = kubernetes.client.StorageV1Api(_get_client())
+    storage_classes = storagev1.list_storage_class()
+    selected_sc = None
+    default_sc_annotations = {
+        "storageclass.kubernetes.io/is-default-class": "true",
+        # Older clusters still use the beta annotation.
+        "storageclass.beta.kubernetes.io/is-default-class": "true",
+    }
+
+    # default to the first if there is no default
+    selected_sc = storage_classes.items[0].metadata.name
+
+    for sc in storage_classes.items:
+        if not selected_sc:
+            # Select the first storage class in case there is no a default-class
+            selected_sc = sc.metadata.name
+        annotations = sc.metadata.annotations
+        if any(
+            k in annotations and annotations[k] == v
+            for k, v in default_sc_annotations.items()
+        ):
+            # Default storage
+            selected_sc = sc.metadata.name
+            break
+    return selected_sc
+
+
+def create_persistent_volume_claim(name, size, logger, storage_class=None, template=None, **kwargs):
     """
     Create a Persistent Volume Claim in the Kubernetes cluster.
     If a PVC with the name given already exists then the function
@@ -31,14 +70,21 @@ def create_persistent_volume_claim(name, size, logger, **kwargs):
     """
     if not size:
         raise Exception(f"Size must be set. Got {size!r}.")
+    
+    if not storage_class:
+        storage_class = _get_default_storage_class()
 
     if not persistent_volume_claim_exists(name):
-        path = os.path.join(os.path.dirname(__file__), 'templates', 'pvc.yaml')
-        tmpl = open(path, 'rt').read()
-        text = tmpl.format(name=name, size=size)
+        if not template:
+            path = os.path.join(os.path.dirname(__file__), 'templates', 'pvc.yaml')
+            template = open(path, 'rt').read()
+        text = template.format(
+            name=name,
+            size=size,
+            storageClass=storage_class)
         data = dict_merge(yaml.safe_load(text), kwargs)
 
-        obj = _get_api().create_namespaced_persistent_volume_claim(
+        obj = _get_corev1_api().create_namespaced_persistent_volume_claim(
             namespace=conf.get_configuration()['namespace'],
             body=data,
         )
@@ -71,7 +117,7 @@ def get_persistent_volume_claim(name):
     Returns:
         The PVC data (see https://kubernetes.io/docs/concepts/storage/persistent-volumes/)
     """
-    foundPVCs = _get_api().list_namespaced_persistent_volume_claim(
+    foundPVCs = _get_corev1_api().list_namespaced_persistent_volume_claim(
         namespace=conf.get_configuration()['namespace'],
         field_selector=f'metadata.name={name}')
     if len(foundPVCs.items) > 0:
@@ -80,6 +126,6 @@ def get_persistent_volume_claim(name):
 
 
 def delete_persistent_volume_claim(name):
-    _get_api().delete_namespaced_persistent_volume_claim(
+    _get_corev1_api().delete_namespaced_persistent_volume_claim(
         name=name,
         namespace=conf.get_configuration()['namespace'])
