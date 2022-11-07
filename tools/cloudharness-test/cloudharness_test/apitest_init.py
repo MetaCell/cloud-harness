b39290f7f3b9a859a6d672d3a17e1eacfed895cd
Filippo Ledda <filippo@metacell.us>
2022-11-07 14:39:01 +0100

    Fix unlogged tests

--- tools/cloudharness-test/cloudharness_test/apitest_init.py
+++ tools/cloudharness-test/cloudharness_test/apitest_init.py
@@ -5,7 +5,7 @@ import schemathesis as st
 
 from cloudharness.auth import get_token
 
-if "APP_URL" in os.environ and "USERNAME" in os.environ and "PASSWORD" in os.environ:
+if "APP_URL" in os.environ:
 
     app_url = os.environ["APP_URL"]
 
@@ -16,16 +16,17 @@ if "APP_URL" in os.environ and "USERNAME" in os.environ and "PASSWORD" in os.env
     except Exception as e:
         raise Exception(f"Cannot setup api tests: {openapi_uri} not reachable. Check your deployment is up and configuration") from e
 
-    @st.auth.register()
-    class TokenAuth:
-        def get(self, context):
-            
-            username = os.environ["USERNAME"]
-            password = os.environ["PASSWORD"]  
-    
-            return get_token(username, password)
+    if "USERNAME" in os.environ and "PASSWORD" in os.environ:
+        @st.auth.register()
+        class TokenAuth:
+            def get(self, context):
+                
+                username = os.environ["USERNAME"]
+                password = os.environ["PASSWORD"]  
+        
+                return get_token(username, password)
 
-        def set(self, case, data, context):
-            case.headers = case.headers or {}
-            case.headers["Authorization"]  = f"Bearer {data}"
-            case.headers["Cookie"]  = f"kc-access={data}"
+            def set(self, case, data, context):
+                case.headers = case.headers or {}
+                case.headers["Authorization"]  = f"Bearer {data}"
+                case.headers["Cookie"]  = f"kc-access={data}"
