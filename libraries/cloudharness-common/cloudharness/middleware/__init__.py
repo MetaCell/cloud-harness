1efc054818f6facac0e72901b4b43175e59c7057
Filippo Ledda <filippo@metacell.us>
2023-03-27 15:33:30 +0200

    Improve stability in auth token
    Some uncontrolled calls in django set the token to None overriding
    the correct value

--- libraries/cloudharness-common/cloudharness/middleware/__init__.py
+++ libraries/cloudharness-common/cloudharness/middleware/__init__.py
@@ -3,7 +3,8 @@ from contextvars import ContextVar, copy_context
 _authentication_token = ContextVar("ch_authentication_token", default=None)
 
 def set_authentication_token(authentication_token):
-    _authentication_token.set(authentication_token)
+    if authentication_token:
+        _authentication_token.set(authentication_token)
 
 def get_authentication_token():
     return _authentication_token.get()
