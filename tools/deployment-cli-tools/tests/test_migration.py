fcb9a05de25dd2ac28ef0418c64b76d3d3f4c0fc
ddelpiano <dario@metacell.us>
2024-02-16 22:17:26 +0100

    required files for unit test

--- tools/deployment-cli-tools/tests/test_migration.py
+++ tools/deployment-cli-tools/tests/test_migration.py
@@ -4,6 +4,10 @@ from ch_cli_tools.migration import perform_migration
 HERE = os.path.dirname(os.path.realpath(__file__)).replace(os.path.sep, '/')
 
 def test_migration_accept_all():
-    assert len(search_word_in_folder(os.path.join(HERE, './resources/migration'), "CLOUDHARNESS_BASE_DEBIAN")) == 2
+    assert len(search_word_in_folder(os.path.join(HERE, './resources/migration/applications'), "CLOUDHARNESS_BASE_DEBIAN")) == 2
     perform_migration(os.path.join(HERE, './resources/migration'), accept_all=True)
-    assert len(search_word_in_folder(os.path.join(HERE, './resources/migration'), "CLOUDHARNESS_BASE_DEBIAN")) == 0
+    assert len(search_word_in_folder(os.path.join(HERE, './resources/migration/applications'), "CLOUDHARNESS_BASE_DEBIAN")) == 0
+    os.system(f'cp -R {os.path.join(HERE, "resources/migration/backup/migration_app")} {os.path.join(HERE, "resources/migration/applications/")}')
+    # print(f'cp -R {os.path.join(HERE, "resources/migration/backup")} {os.path.join(HERE, "resources/migration/applications")}')
+    # subprocess.Popen(['cp', '-R', 'backup/*', 'applications/'], cwd=os.path.join(HERE, 'resources/migration'))
+    assert len(search_word_in_folder(os.path.join(HERE, './resources/migration/applications'), "CLOUDHARNESS_BASE_DEBIAN")) == 2
