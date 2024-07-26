bad0be01957a497d633eea18258f31e67e6523ef
Filippo Ledda <filippo@metacell.us>
2024-07-26 19:00:11 +0200

    CH-140 update python dependencies

--- libraries/cloudharness-common/setup.py
+++ libraries/cloudharness-common/setup.py
@@ -12,19 +12,24 @@ VERSION = "2.3.0"
 # http://pypi.python.org/pypi/setuptools
 
 REQUIREMENTS = [
-    'kubernetes',
-    'kafka-python',
-    'pyaml',
+    'kubernetes >= 29.0.0',
+    'PyYAML >= 6.0.1',
+    'oyaml >= 1.0',
     'pyjwt>=2.6.0',
     'cryptography',
     'requests>=2.21.0',
     'sentry-sdk[flask]>=0.14.4',
-    'python-keycloak',
+    'python-keycloak >= 3.7.0',
     'cloudharness_model',
     'argo-workflows==5.0.0',
-    'cachetools'
+    'cachetools >= 5.3.2',
+    'blinker >= 1.7.0',
+    'jinja2 >= 3.1.4',
+    'kafka-python >= 2.0.2',
+    'requests >= 2.31.0',
+    'python-dateutil >= 2.8.2',
+    'sentry-sdk >= 1.39.2',
 ]
-
 
 
 setup(
@@ -35,7 +40,8 @@ setup(
     url="",
     keywords=["cloudharness", "cloud"],
     install_requires=REQUIREMENTS,
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     package_data={'': ['*.yaml']},
     long_description="""\
--- libraries/models/setup.py
+++ libraries/models/setup.py
@@ -10,11 +10,15 @@ HERE = dn(realpath(__file__))
 NAME = "cloudharness_model"
 VERSION = "2.3.0"
 REQUIREMENTS = [
-    "swagger-ui-bundle >= 0.0.2",
-    "python_dateutil >= 2.6.0",
-    "setuptools >= 21.0.0",
-    "pyhumps",
-    "oyaml"
+    "Jinja2 >= 3.1.3",
+    "oyaml >= 1.0",
+    "psutil >= 5.9.4",
+    "pyhumps >= 3.8.0",
+    "python-dateutil >= 2.8.2",
+    "PyYAML >= 6.0.1",
+    "six >= 1.16.0",
+    "swagger_ui_bundle >= 1.1.0",
 ]
 print(REQUIREMENTS)
-setup(name=NAME, version=VERSION, install_requires=REQUIREMENTS, packages=find_packages(),)
+setup(name=NAME, version=VERSION,
+      install_requires=REQUIREMENTS, packages=find_packages(),)
