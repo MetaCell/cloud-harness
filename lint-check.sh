8e0ff82b797aa218196eff9c3fa6ead031d805ea
Filippo Ledda <filippo@metacell.us>
2024-09-09 17:11:53 +0200

    CH-147 linting automation

--- lint-check.sh
+++ lint-check.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Run autopep8 with --diff and capture the output
-diff_output=$(autopep8 --select=E1,E2,E3,W,E4,E7,E502  --recursive --diff\
- --exclude '**/cloudharness_cli/**/*'\
- --exclude '**/models/*'\
- --exclude '**/model/*'\
- .)
+diff_output=$(autopep8 --select=E1,E2,E3,W,E4,E7,E502  --recursive --diff --exclude '**/cloudharness_cli/**/*,**/models/*,**/model/*' .)
 # Check if the output is non-empty
 if [ -n "$diff_output" ]; then
     echo $diff_output
