1562e95ba0cfb40324156b57f8f25038bd099e32
Filippo Ledda <filippo@metacell.us>
2024-09-16 19:17:38 +0200

    CH-17 fix broken API tests

--- libraries/cloudharness-common/tests/test_infrastructure.py
+++ libraries/cloudharness-common/tests/test_infrastructure.py
@@ -1,8 +1,8 @@
-from cloudharness.infrastructure import k8s
 from .test_env import set_test_environment
 
 set_test_environment()
 
+from cloudharness.infrastructure import k8s  # noqa: E402
 
 kubectl_enabled = False
 
