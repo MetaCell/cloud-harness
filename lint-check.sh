8bb9a1b06e2059aa0b6d6f77725ebbb73dd853af
Filippo Ledda <filippo@metacell.us>
2024-09-17 18:14:44 +0200

    Linting fix

--- lint-check.sh
+++ lint-check.sh
@@ -5,6 +5,6 @@ diff_output=$(python -m autopep8 --select=E1,E2,E3,W,E4,E7,E502  --recursive --d
 if [ -n "$diff_output" ]; then
     printf "%s\n" "$diff_output"
     echo "Code style issues found in the above files. To fix you can run: "
-    echo "autopep8 --select=E1,E2,E3,W,E4,E7,E502  --recursive  --in-place --exclude '**/cloudharness_cli/**/*,**/models/*,**/model/*'"
+    echo "autopep8 --select=E1,E2,E3,W,E4,E7,E502  --recursive  --in-place --exclude '**/cloudharness_cli/**/*,**/models/*,**/model/*' ."
     exit 1
 fi
