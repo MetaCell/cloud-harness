9789fe294990cb15a877762d46ad1e05ba6da505
Jake Conkerton-Darby <jake@metacell.us>
2024-11-12 15:29:17 +0000

    CH-159 Add admin role to the api_admin user

--- applications/accounts/scripts/create_api_user.sh
+++ applications/accounts/scripts/create_api_user.sh
@@ -10,3 +10,4 @@ echo Creating API user
 # create the user and reload keycloak
 /opt/keycloak/bin/kcadm.sh create users -s "username=$USERNAME" -s enabled=True
 /opt/keycloak/bin/kcadm.sh set-password --username "$USERNAME" --new-password "$PASSWORD"
+/opt/keycloak/bin/kcadm.sh add-roles --uusername "$USERNAME" --rolename admin
