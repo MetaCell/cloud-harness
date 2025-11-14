055d9b341abed18180fdcf4018225e3577963832
Filippo Ledda <filippo@metacell.us>
2025-11-14 19:27:43 +0100

    CH-226 fix common service

--- applications/common/server/common/__main__.py
+++ applications/common/server/common/__main__.py
@@ -3,17 +3,10 @@
 from cloudharness.utils.server import init_flask, main
 from cloudharness import log
 from common.repository.db import open_db
-from common.controllers.sentry_controller import global_dsn
 
 
-def init_fn(app):
-    log.info("initializing database from app")
-    # CORS is now handled by the init_flask function
-    if not global_dsn:
-        open_db(app)
 
-
-app = init_flask(init_app_fn=init_fn)
+app = init_flask()
 
 if __name__ == '__main__':
     main()
