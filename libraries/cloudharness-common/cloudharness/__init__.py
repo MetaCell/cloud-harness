b0c173f108a319b8a0ad34c6e7644206d247a5e6
Filippo Ledda <filippo@metacell.us>
2025-11-14 19:12:38 +0100

    CH-226 JSON encoder  fix

--- libraries/cloudharness-common/cloudharness/__init__.py
+++ libraries/cloudharness-common/cloudharness/__init__.py
@@ -16,6 +16,32 @@ def set_debug():
 # TODO log will write through a rest service
 
 
+json_dumps = js.dumps
+
+
+def dumps(o, *args, **kwargs):
+    try:
+        if "cls" not in kwargs:
+            return json_dumps(o, cls=CloudHarnessJSONEncoder, *args, **kwargs)
+        return json_dumps(o, *args, **kwargs)
+    except TypeError as e:
+        # If serialization fails, try converting objects with to_dict method
+        if "not JSON serializable" in str(e):
+            if hasattr(o, "to_dict") and callable(getattr(o, "to_dict")):
+                o = o.to_dict()
+                return json_dumps(o, *args, **kwargs)
+            # Handle lists/tuples of objects with to_dict
+            if isinstance(o, (list, tuple)):
+                converted = [item.to_dict() if hasattr(item, "to_dict") and callable(getattr(item, "to_dict")) else item for item in o]
+                return json_dumps(converted, *args, **kwargs)
+        # If we still can't serialize, try without cls parameter
+        if "cls" in kwargs:
+            kwargs_no_cls = {k: v for k, v in kwargs.items() if k != "cls"}
+            return json_dumps(o, *args, **kwargs_no_cls)
+        raise
+
+
+
 
 class NotCorrectlyInitialized(Exception):
     pass
