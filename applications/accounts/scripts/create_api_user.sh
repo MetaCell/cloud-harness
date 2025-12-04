c0f0401723d4601b19ced2067db3cc661d0e78ce
Filippo Ledda <filippo@metacell.us>
2025-12-04 16:59:16 +0100

    CH-231 fix update password

--- applications/accounts/scripts/create_api_user.sh
+++ applications/accounts/scripts/create_api_user.sh
@@ -4,12 +4,12 @@ NAMESPACE=${CH_ACCOUNTS_REALM}
 USERNAME=admin_api
 PASSWORD=$(cat /opt/cloudharness/resources/auth/api_user_password)
 
-set -e
 echo "Checking if API user exists..."
 
 # Check if user already exists
 if /opt/keycloak/bin/kcadm.sh get users -q "username=$USERNAME" | grep -q "$USERNAME"; then
-    echo "API user $USERNAME already exists, skipping creation"
+    echo "API user $USERNAME already exists, syncing password to the one in the secret"
+    /opt/keycloak/bin/kcadm.sh set-password --username "$USERNAME" --new-password "$PASSWORD"
     exit 0
 fi
 
@@ -17,7 +17,7 @@ echo "Creating API user $USERNAME"
 
 # create the user and reload keycloak
 /opt/keycloak/bin/kcadm.sh create users -s "username=$USERNAME" -s enabled=True
-/opt/keycloak/bin/kcadm.sh set-password --username "$USERNAME" --new-password "$PASSWORD"
+
 /opt/keycloak/bin/kcadm.sh add-roles --uusername "$USERNAME" --rolename admin
 
 echo "API user created successfully"
