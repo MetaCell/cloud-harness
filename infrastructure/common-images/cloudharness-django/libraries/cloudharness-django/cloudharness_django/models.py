09bd7d6a40671fd820dcad644ca41acb104ff17a
D. Gopal Krishna <nikhil0223@gmail.com>
2026-01-28 10:41:41 +0530

    CH-220 - add organization and organization member models and its migrations

--- infrastructure/common-images/cloudharness-django/libraries/cloudharness-django/cloudharness_django/models.py
+++ infrastructure/common-images/cloudharness-django/libraries/cloudharness-django/cloudharness_django/models.py
@@ -19,3 +19,27 @@ class Member(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=256)
+    kc_id = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    class Meta:
+        verbose_name = "CloudHarness Organization"
+        verbose_name_plural = "CloudHarness Organizations"
+
+
+class OrganizationMember(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} - {self.organization}"
+
+    class Meta:
+        verbose_name = "CloudHarness Organization Member"
+        verbose_name_plural = "CloudHarness Organization Members"
