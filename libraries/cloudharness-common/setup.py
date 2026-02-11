d35fcc897953b4cf3e3d5da91dc51cc22d53f958
Filippo Ledda <filippo@metacell.us>
2026-02-11 16:22:25 +0100

    update libraries version numbers

--- libraries/cloudharness-common/setup.py
+++ libraries/cloudharness-common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 NAME = "cloudharness"
-VERSION = "2.5.0"
+VERSION = "3.0.0"
 # To install the library, run the following
 #
 # python setup.py install
