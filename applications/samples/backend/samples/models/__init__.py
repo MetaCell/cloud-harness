5274ec8c51de13980997db21a75a17505cdc21f5
Filippo Ledda <filippo@metacell.us>
2026-03-10 10:31:25 +0100

    chore: linting and test related fixes

--- applications/samples/backend/samples/models/__init__.py
+++ applications/samples/backend/samples/models/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 # import models into model package
-from samples.models.get_db_connect_string200_response import GetDbConnectString200Response
 from samples.models.inline_response202 import InlineResponse202
 from samples.models.inline_response202_task import InlineResponse202Task
 from samples.models.sample_resource import SampleResource
