bea94170c769a23bfa53351107c45b11a73b989a
Filippo Ledda <filippo@metacell.us>
2026-09-01 11:27:34 +0200

    Regenerate samples application to cover new API

--- applications/samples/backend/samples/models/__init__.py
+++ applications/samples/backend/samples/models/__init__.py
@@ -3,3 +3,5 @@
 from samples.models.inline_response202 import InlineResponse202
 from samples.models.inline_response202_task import InlineResponse202Task
 from samples.models.sample_resource import SampleResource
+from samples.models.write_file200_response import WriteFile200Response
+from samples.models.write_file_request import WriteFileRequest
--- applications/samples/frontend/src/rest/samples/apis/index.ts
+++ applications/samples/frontend/src/rest/samples/apis/index.ts
@@ -1,6 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './AuthApi';
+export * from './DatabaseApi';
 export * from './ResourceApi';
 export * from './TestApi';
 export * from './WorkflowsApi';
